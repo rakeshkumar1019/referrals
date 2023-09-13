405191ee2608fa733ea3f374feaa940cfbfe5a65
srakesh1019 <srakeshkumar1019@gmail.com>
2023-09-13 11:45:59 +0530

    context path issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import Context from "./context/context.jsx";
+import Context from "./context/Context.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Context>
